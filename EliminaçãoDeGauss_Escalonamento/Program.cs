70d4e4875d8e56986b28fc75e2997945e1f21ec2
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-04-29 17:40:18 -0300

    Comentários

--- EliminaçãoDeGauss_Escalonamento/Program.cs
+++ EliminaçãoDeGauss_Escalonamento/Program.cs
@@ -8,11 +8,13 @@ namespace EliminaçãoDeGauss_Escalonamento
     {
         static void Main(string[] args)
         {
+            //definindo variaveis
             int N = 0;
             float[] b = new float[30];
             float[] x = new float[30];
             float[,] a = new float[30, 30];
 
+            //coletando dados
             Console.Write("Digite a ordem do sistema: ");
             N = Convert.ToInt32(Console.ReadLine());
 
@@ -23,6 +25,18 @@ namespace EliminaçãoDeGauss_Escalonamento
                 {
                     Console.WriteLine($"Coluna: {j+1}");
                     a[i, j] = float.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            //apresentando matriz
+            Console.WriteLine("Nossa Matriz: ");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(a[i,j]);
                 }
                 Console.WriteLine();
             }
