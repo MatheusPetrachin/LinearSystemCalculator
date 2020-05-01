732c8971cb07073768636a3c33c804ab3ba9ecbe
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-04-30 23:40:31 -0300

    Finalizando projeto

--- EliminaçãoDeGauss_Escalonamento/Program.cs
+++ EliminaçãoDeGauss_Escalonamento/Program.cs
@@ -6,31 +6,49 @@ namespace EliminaçãoDeGauss_Escalonamento
 {
     class Program
     {
+        static int N = 0;
+        static float S = 0;
+        static float[] b = new float[30];
+        static float[] x = new float[30];
+        static float[,] a = new float[30, 30];
+
         static void Main(string[] args)
         {
-            //definindo variaveis
-            int N = 0;
-            float[] b = new float[30];
-            float[] x = new float[30];
-            float[,] a = new float[30, 30];
+            LeMatriz();
+            Triangular();
+            Solucao();
+            Resultado();
+            Console.ReadLine();
+            
+        }
 
-
+        public static void LeMatriz()
+        {
             Console.Write("Digite a ordem do sistema: ");
             N = Convert.ToInt32(Console.ReadLine());
 
-            //etodosLeMatriz(N);
-            
-                                   
-
-            
-        }        
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Linha: {i + 1}");
+                for (int j = 0; j < N; j++)
+                {
+                    Console.WriteLine($"Coluna: {j + 1}");
+                    a[i, j] = float.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("B: ");
+                b[i] = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            Show();
+            Console.ReadLine();
+        }
 
         public static float Det(float a, float b, float c, float d)
         {
             return (a * d) - (b * c);
         }
 
-        public static void Triangularizacao(int N, float[,] a, float[] b)
+        public static void Triangular()
         {
             int k, i, j;
 
@@ -41,14 +59,36 @@ namespace EliminaçãoDeGauss_Escalonamento
                     for (j = (k + 1); j < N; j++)
                     {
                         a[i, j] = Det(a[k, k], a[i, k], a[k, j], a[i, j]);
+                        Show();
                     }
                     b[i] = Det(a[k, k], a[i, k], b[k], b[i]);
                     a[i, k] = 0;
+                    Show();
                 }
             }
         }
 
-        public static void Show(int N, float[,] a, float[] b)
+        public static void Solucao()
+        {
+            x[N - 1] = b[N - 1] / a[N - 1, N - 1];
+            for (int k = N - 2; k >= 0; k--)
+            {
+                S = 0;
+                for (int j = k + 1; j < N; j++)
+                    S += a[k, j] * x[j];
+                x[k] = (b[k] - S) / a[k, k];
+            }
+        }
+
+        public static void Resultado()
+        {
+            Console.WriteLine("Solução do Problema");
+            for (int i = 0; i < N; i++)
+                Console.WriteLine(" {0} ", Math.Round(x[i], 3));
+        }
+
+
+        public static void Show()
         {
             Console.WriteLine("Nossa Matriz: ");
             for (int i = 0; i < N; i++)
@@ -62,21 +102,5 @@ namespace EliminaçãoDeGauss_Escalonamento
             Console.ReadLine();
         }
 
-        public static void LeMatriz(int N, float[,] a, float[] b)
-        {
-            for (int i = 0; i < N; i++)
-            {
-                Console.WriteLine($"Linha: {i + 1}");
-                for (int j = 0; j < N; j++)
-                {
-                    Console.WriteLine($"Coluna: {j + 1}");
-                    a[i, j] = float.Parse(Console.ReadLine());
-                }
-                Console.WriteLine("B: ");
-                b[i] = float.Parse(Console.ReadLine());
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
     }
 }
